52868c142e23b55e2ac91a9e5c5ffe14520f923b
Riad <felihriad5@gmail.com>
2022-04-25 00:49:45 +0200

    some fixes

--- config.php
+++ config.php
@@ -3,12 +3,12 @@
 declare(strict_types=1);
 
 
-use Dotenv\Dotenv;
+// use Dotenv\Dotenv;
 
 require './vendor/autoload.php';
 
-$dotenv = Dotenv::createImmutable('./');
-$dotenv->load();
+// $dotenv = Dotenv::createImmutable('./');
+// $dotenv->load();
 
 $ROOT_PATH =  $_SERVER['DOCUMENT_ROOT'] . '/blog';
 // database
@@ -18,8 +18,8 @@ const DB_PASS = '';
 const DB_NAME = 'blog-site';
 const USERS_TABLE = 'users';
 //mailing
-$SENDER = getenv('SNEDER');
-$SENDER_PASS = getenv('SENDER_PASS');
+// $SENDER = getenv('SNEDER');
+// $SENDER_PASS = getenv('SENDER_PASS');
 //others
 const SITE_NAME = 'readdaily';
 const SITE_PATH = 'http://localhost:8080/blog';
